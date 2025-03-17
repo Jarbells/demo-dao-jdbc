d43f7a2689124f6cd9f2287555c0a07920dce1cd
Jarbas Santos <jarbells@outlook.com>
2025-03-17 17:17:56 -0300

    Department deleteById

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,16 +1,15 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
-import model.entities.Department;
-import model.entities.Seller;
 
 public class Program2 {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 	
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
@@ -35,10 +34,21 @@ public class Program2 {
 		System.out.println("Update completed");
 		*/
 		
+		/*
 		System.out.println("\n=== TEST 4: department findAll ====");
 		List<Department> listDepartment = new ArrayList<>(departmentDao.findAll());
 		for (Department obj : listDepartment) {
 			System.out.println(obj);
-		}		
+		}
+		*/		
+		
+		/*
+		System.out.println("\n=== TEST 4: department deleteById ====");
+		System.out.print("Digite um ID para deletar um departamento: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Departamento " + id + " Deletado com sucesso!");
+		*/
+		sc.close();
 	}
 }
